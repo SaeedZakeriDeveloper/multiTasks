beaebfe3a6dc60fa2f75391f1f1fe22cfeade6c0
saeedzakeri1364 <saeed.zakeri.dev@gmail.com>
2025-06-04 14:17:26 -0400

    doCrudCode

--- note.ts
+++ note.ts
@@ -5,3 +5,16 @@
 //UPDATE
 //DELETE
 
+
+//add
+//vaghti roye button add click mikonim ham bayad control name ha reset beshe va ham intity 
+//id hamishe serf pass dade mishe
+//bad az inke add anjam shod res ro bayad be list ezafe konim 
+
+//update
+//vaghti ro button clicked mikonim recordi ke rosh click mikonim ro mirizim to entity 
+
+
+
+
+
